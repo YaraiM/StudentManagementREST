156b5719ad54553757e8423358e0dc06597c3c1a
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-07-15 00:14:26 +0900

    StudentControllerの単体テスト作成：受講生新規登録およびリクエストの入力チェック

--- src/test/java/raisetech/student/management/controller/StudentControllerTest.java
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -3,23 +3,28 @@ package raisetech.student.management.controller;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static raisetech.student.management.model.data.Gender.男性;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import raisetech.student.management.model.data.Student;
 import raisetech.student.management.model.data.StudentCourse;
@@ -38,6 +43,28 @@ class StudentControllerTest {
 
   @MockBean
   private StudentService service;
+
+  private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+  /**
+   * テスト用にStudentDetailオブジェクトを作成するメソッドです。受講生IDのみ入力したインスタンスが生成されます。
+   *
+   * @param id 受講生ID
+   * @return 受講生の詳細情報
+   */
+  private static StudentDetail createTestStudentDetail(int id) {
+    Student student = new Student();
+    student.setId(id);
+
+    StudentCourse studentCourse1 = new StudentCourse();
+    StudentCourse studentCourse2 = new StudentCourse();
+    studentCourse1.setStudentId(student.getId());
+    studentCourse2.setStudentId(student.getId());
+    List<StudentCourse> studentCourses = new ArrayList<>(List.of(studentCourse1, studentCourse2));
+
+    StudentDetail studentDetail = new StudentDetail(student, studentCourses);
+    return studentDetail;
+  }
 
   @Test
   void 受講生詳細の一覧検索_エンドポイントでサービスの処理が適切に呼び出されて処理成功のレスポンスが返ってくること()
@@ -64,21 +91,11 @@ class StudentControllerTest {
       throws Exception {
     // 事前準備
     int id = 666;
-    Student student = new Student();
-    student.setId(id);
-
-    StudentCourse studentCourse1 = new StudentCourse();
-    StudentCourse studentCourse2 = new StudentCourse();
-    studentCourse1.setStudentId(student.getId());
-    studentCourse2.setStudentId(student.getId());
-    List<StudentCourse> studentCourses = new ArrayList<>(List.of(studentCourse1, studentCourse2));
-
-    StudentDetail studentDetail = new StudentDetail(student, studentCourses);
+    StudentDetail studentDetail = createTestStudentDetail(id);
     when(service.searchStudent(id)).thenReturn(studentDetail);
 
     // 実行と検証
-    ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/students/detail")
-            .param("id", String.valueOf(id)))
+    mockMvc.perform(MockMvcRequestBuilders.get("/students/detail").param("id", String.valueOf(id)))
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.student.id").value(id))
@@ -107,6 +124,110 @@ class StudentControllerTest {
             result.getResolvedException().getMessage()));
 
     verify(service, times(1)).searchStudent(id);
+  }
+
+  @Test
+  void 受講生の新規登録_エンドポイントでサービスの処理が適切に呼び出されリクエスト情報に基づくstudentDetailが返ってくること()
+      throws Exception {
+    // 事前準備
+    int id = 666;
+    StudentDetail studentDetail = createTestStudentDetail(id);
+
+    studentDetail.getStudent().setFullname("田中昭三");
+    studentDetail.getStudent().setFurigana("たなかしょうぞう");
+    studentDetail.getStudent().setNickname("ショーゾー");
+    studentDetail.getStudent().setMail("shozo@example.com");
+    studentDetail.getStudent().setAddress("東京");
+    studentDetail.getStudent().setAge(55);
+    studentDetail.getStudent().setGender(男性);
+    studentDetail.getStudent().setRemark("新規登録のテストです");
+    studentDetail.getStudent().setDeleted(false);
+
+    studentDetail.getStudentCourses().get(0).setCourseName("Java");
+    studentDetail.getStudentCourses().get(1).setCourseName("Ruby");
+
+    when(service.registerStudent(any(StudentDetail.class))).thenReturn(studentDetail);
+
+    String jsonRequest = objectMapper.writeValueAsString(studentDetail);
+
+    // 実行と検証
+    mockMvc.perform(MockMvcRequestBuilders.post("/students/new")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonRequest))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.student.id").value(id))
+        .andExpect(jsonPath("$.student.fullname").value("田中昭三"))
+        .andExpect(jsonPath("$.student.furigana").value("たなかしょうぞう"))
+        .andExpect(jsonPath("$.student.nickname").value("ショーゾー"))
+        .andExpect(jsonPath("$.student.mail").value("shozo@example.com"))
+        .andExpect(jsonPath("$.student.address").value("東京"))
+        .andExpect(jsonPath("$.student.age").value(55))
+        .andExpect(jsonPath("$.student.gender").value("男性"))
+        .andExpect(jsonPath("$.student.remark").value("新規登録のテストです"))
+        .andExpect(jsonPath("$.student.deleted").value(false))
+        .andExpect(jsonPath("$.studentCourses", hasSize(2)))
+        .andExpect(jsonPath("$.studentCourses[0].studentId").value(id))
+        .andExpect(jsonPath("$.studentCourses[0].courseName").value("Java"))
+        .andExpect(jsonPath("$.studentCourses[1].studentId").value(id))
+        .andExpect(jsonPath("$.studentCourses[1].courseName").value("Ruby"));
+
+    verify(service, times(1)).registerStudent(any(StudentDetail.class));
+
+  }
+
+  @Test
+  void 入力チェック_リクエスト可能な情報がすべて適切な場合に入力チェックがかからないこと()
+      throws Exception {
+    // 事前準備
+    int id = 666;
+    StudentDetail studentDetail = createTestStudentDetail(id);
+
+    studentDetail.getStudent().setFullname("田中昭三");
+    studentDetail.getStudent().setFurigana("たなかしょうぞう");
+    studentDetail.getStudent().setNickname("ショーゾー");
+    studentDetail.getStudent().setMail("shozo@example.com");
+    studentDetail.getStudent().setAddress("東京");
+    studentDetail.getStudent().setAge(55);
+    studentDetail.getStudent().setGender(男性);
+    studentDetail.getStudent().setRemark("新規登録のテストです");
+    studentDetail.getStudent().setDeleted(false);
+
+    studentDetail.getStudentCourses().get(0).setCourseName("Java");
+    studentDetail.getStudentCourses().get(1).setCourseName("Ruby");
+
+    Set<ConstraintViolation<StudentDetail>> violations = validator.validate(studentDetail);
+
+    // 検証
+    assertEquals(0, violations.size());
+
+  }
+
+  @Test
+  void 入力チェック_リクエスト可能な情報のうち入力値の指定がある項目が不適切な場合に入力チェックがかかること()
+      throws Exception {
+    // 事前準備
+    int id = 666;
+    StudentDetail studentDetail = createTestStudentDetail(id);
+
+    studentDetail.getStudent().setFullname(""); // fullnameは空白を許容しない
+    studentDetail.getStudent().setFurigana(""); // fuliganaは空白を許容しない
+    studentDetail.getStudent().setNickname("ショーゾー");
+    studentDetail.getStudent().setMail("shozo&example.com"); // mailはメールアドレス以外の入力値を許容しない
+    studentDetail.getStudent().setAddress("東京");
+    studentDetail.getStudent().setAge(55);
+    studentDetail.getStudent().setGender(男性);
+    studentDetail.getStudent().setRemark("新規登録のテストです");
+    studentDetail.getStudent().setDeleted(false);
+
+    studentDetail.getStudentCourses().get(0).setCourseName(""); // courseNameは空白を許容しない
+    studentDetail.getStudentCourses().get(1).setCourseName(""); // courseNameは空白を許容しない
+
+    Set<ConstraintViolation<StudentDetail>> violations = validator.validate(studentDetail);
+
+    // 検証
+    assertEquals(5, violations.size());
+
   }
 
 }
