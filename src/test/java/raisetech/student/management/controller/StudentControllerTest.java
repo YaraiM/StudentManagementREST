17b1b384b1f1aef698b4e6419cfebf3d546c0c5d
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-07-14 16:26:39 +0900

    StudentControllerの単体テスト作成：受講生一覧検索機能

---
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -0,0 +1,34 @@
+package raisetech.student.management.controller;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import raisetech.student.management.model.services.StudentService;
+
+@WebMvcTest(StudentController.class) //Spring MVCのうち、Web層（特にコントローラ層）の単体テストに使用される
+class StudentControllerTest {
+
+  @Autowired
+  MockMvc mockMvc; //Spring MVCのエンドポイントをテストするためのモックオブジェクト
+
+  @MockBean
+  private StudentService service;
+
+  @Test
+  void 受講生詳細の一覧検索_エンドポイントでサービスの処理が適切に呼び出されていること()
+      throws Exception {
+    // 実行と検証
+    mockMvc.perform(MockMvcRequestBuilders.get("/students"))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).searchStudentList();
+  }
+
+}
