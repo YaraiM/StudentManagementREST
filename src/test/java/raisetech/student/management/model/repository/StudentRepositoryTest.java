715d0d9b1aa1dc5fb0fa45c26e934462a62d3325
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-07-20 14:26:03 +0900

    StudentRepositoryの単体テスト：外部ライブラリ、事前設定

---
+++ src/test/java/raisetech/student/management/model/repository/StudentRepositoryTest.java
@@ -0,0 +1,14 @@
+package raisetech.student.management.model.repository;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+
+@MybatisTest
+class StudentRepositoryTest {
+
+  @Test
+  void test() {
+
+  }
+
+}
