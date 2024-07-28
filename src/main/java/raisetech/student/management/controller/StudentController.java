e31cb71bce8b1ac12db600c73766846e265030d3
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-07-28 23:26:33 +0900

    コース申込状況の管理機能：コントローラーの実装と単体テスト

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.student.management.model.data.CourseStatus;
 import raisetech.student.management.model.domain.CourseDetail;
 import raisetech.student.management.model.domain.IntegratedDetail;
 import raisetech.student.management.model.domain.StudentDetail;
@@ -61,10 +62,31 @@ public class StudentController {
   }
 
   /**
+   * 受講生コースの一覧検索です。コースの申込状況を確認できます。
+   *
+   * @return 受講生コース詳細情報一覧
+   */
+  @Operation(summary = "受講生コース一覧検索", description = "条件に合致する受講生コースの一覧を検索します。")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "処理が成功した場合のレスポンス",
+          content = @Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = CourseDetail.class))
+          )
+      )
+  })
+  @GetMapping("/students/courses")
+  public List<CourseDetail> getStudentCoursesList() {
+
+    return service.searchStudentCourseList();
+
+  }
+
+  /**
    * 受講生の詳細情報の検索です。 IDに紐づく任意の受講生の詳細情報を取得します。
    *
    * @param id 受講生ID
    * @return 受講生IDに紐づく受講生の詳細情報
+   * @throws ResourceNotFoundException 存在しないIDを指定した場合の例外
    */
   @Operation(summary = "受講生の詳細情報検索", description = "IDに紐づく任意の受講生の詳細情報を取得します。")
   @ApiResponses(value = {
@@ -83,22 +105,45 @@ public class StudentController {
   }
 
   /**
-   * 受講生の詳細情報の新規登録です。
+   * 受講生コースの詳細情報の検索です。 IDに紐づく任意のコースの申込状況を取得します。
+   *
+   * @param id 受講生コースID
+   * @return 受講生コースの詳細情報（申込状況）
+   * @throws ResourceNotFoundException 存在しないIDを指定した場合の例外
+   */
+  @Operation(summary = "受講生コースの詳細情報検索", description = "IDに紐づく任意の受講生コースの詳細情報を取得します。")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "処理が成功した場合のレスポンス",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = CourseDetail.class))
+      ),
+      @ApiResponse(responseCode = "404", description = "存在しないIDを指定した場合のレスポンス",
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+      )
+  })
+  @GetMapping("/students/courses/detail")
+  public CourseDetail getStudentCourses(
+      @Parameter(description = "受講生コースのID") @RequestParam @NotNull int id)
+      throws ResourceNotFoundException {
+    return service.searchStudentCourse(id);
+  }
+
+  /**
+   * 受講生の詳細情報の新規登録です。コースの申込状況は「仮登録」として自動登録されます。
    *
    * @param studentDetail 受講生の詳細情報
-   * @return 新規登録が成功した受講生の詳細情報
+   * @return 新規登録が成功した受講生の詳細情報およびコース詳細情報が統合された情報
    */
   @Operation(summary = "受講生新規登録", description = "受講生の詳細情報を新規登録します。")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "処理が成功した場合のレスポンス",
-          content = @Content(mediaType = "application/json", schema = @Schema(implementation = StudentDetail.class))
+          content = @Content(mediaType = "application/json", schema = @Schema(implementation = IntegratedDetail.class))
       )
   })
   @PostMapping("/students/new")
   public ResponseEntity<IntegratedDetail> registerStudent(
-      @RequestBody @Valid StudentDetail studentDetail, List<CourseDetail> courseDetails) {
+      @RequestBody @Valid StudentDetail studentDetail) {
     IntegratedDetail newStudent = service.registerStudent(studentDetail);
-    return ResponseEntity.ok(new IntegratedDetail());
+    return ResponseEntity.ok(newStudent);
   }
 
   /**
@@ -116,6 +161,24 @@ public class StudentController {
   @PutMapping("/students/update")
   public ResponseEntity<String> updateStudent(@RequestBody @Valid StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
+    return ResponseEntity.ok("更新処理が成功しました");
+  }
+
+  /**
+   * 受講生コースの申込状況の更新です。
+   *
+   * @param courseStatus 受講生コースの申込状況
+   * @return 更新が成功した場合に「更新処理が成功しました」と表示
+   */
+  @Operation(summary = "受講生コース申込状況の更新", description = "受講生コースの申込状況を更新します。")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "処理が成功した場合のレスポンス",
+          content = @Content(mediaType = "text/plain", schema = @Schema(type = "string", example = "更新処理が成功しました"))
+      )
+  })
+  @PutMapping("/students/courses/statuses/update")
+  public ResponseEntity<String> updateStudentCourse(@RequestBody @Valid CourseStatus courseStatus) {
+    service.updateCourseStatus(courseStatus);
     return ResponseEntity.ok("更新処理が成功しました");
   }
 
